060d1176d7e1684df54b90342ff843455cc1a1d5
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-18 08:48:26 +0300

    add: get user

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as http from 'http';
-import * as url from 'url';
+
 const PORT = 3000;
 const server = http.createServer();
 
@@ -19,17 +19,26 @@ const users = [
 ];
 
 server.on('request', (req, res) => {
-    const reqUrl = req.url;
+    const reqUrl: string | undefined = req.url;
     const items: any = req.url?.split('/');
-    // /users/2 => ['', 'user', '2']
-
-    console.log(reqUrl);
 
     switch (true) {
         case reqUrl === '/api/users':
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(users));
+            break;
+
+        case `/${items[1]}/${items[2]}/` === '/api/users/' &&
+            items.length === 4:
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+
+            const userIndex: number = Number(items[3]);
+            //todo error
+            if (userIndex >= users.length) console.log('error');
+
+            res.end(JSON.stringify(users[userIndex]));
 
             break;
 
@@ -43,3 +52,28 @@ server.on('request', (req, res) => {
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+//     if (items[1] === 'users') {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         if (items.length === 3) {
+//             const userIndex: number = Number(items[2]);
+//             res.end(JSON.stringify(users[userIndex]));
+//         } else {
+//             res.end(JSON.stringify(users));
+//         }
+//     } else if (items[1] === '/messages') {
+//         res.setHeader('Content-Type', 'text/html');
+//         res.write('<html>');
+//         res.write('<body>');
+//         res.write('<ul>');
+//         res.write('<li>test</li>');
+//         res.write('<li>test2</li>');
+//         res.write('</ul>');
+//         res.write('</body>');
+//         res.write('</html>');
+//         res.end();
+//     } else {
+//         res.statusCode = 404;
+//         res.end();
+//     }
